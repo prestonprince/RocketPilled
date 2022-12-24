75c38c5732703409eef6aa802766db8648cbb9f7
preston <prestonprince18@gmail.com>
2022-12-23 17:54:59 -0700

    finished routes for matches

--- app/api/match_routes.py
+++ app/api/match_routes.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 from app.models import db, Team, User, Match
 from .auth_routes import validation_errors_to_error_messages, authorized
 from ..utils.match_utils import random_map
-from app.forms import PostMatchForm
+from app.forms import PostMatchForm, DeleteMatchForm
 
 
 match_routes = Blueprint('matches', __name__)
@@ -41,3 +41,36 @@ def post_match():
 
         return new_match.to_dict(), 201
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+#TODO after 3rd feature
+# @match_routes.route("/<int:match_id>", methods=['PUT'])
+# def update_status(match_id):
+#     pass
+
+
+@match_routes.route("/<int:match_id>", methods=['DELETE'])
+def cancel_match(match_id):
+    """
+    Query for match by match id and delete it only if status is 'posted'
+    """
+    form = DeleteMatchForm()
+    data = form.data
+
+    match = Match.query.get(match_id)
+    team = Team.query.get(data['team_id'])
+
+    if not match:
+        return {'error': 'Match not found'}
+
+    if match.status != 'posted':
+        return {'error': 'Match cannot be cancelled once accepted'}
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        if team not in match.teams:
+            return {'error': "This team is unauthorized to cancel the match"}
+
+        db.session.delete(match)
+        db.session.commit()
+        return {"message": "Successfully cancelled"}
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -4,3 +4,4 @@ from .create_team_form import CreateTeamForm
 from .add_team_member_form import AddTeamMemberForm
 from .remove_team_member_form import RemoveTeamMemberForm
 from .post_match_form import PostMatchForm
+from .delete_match_form import DeleteMatchForm

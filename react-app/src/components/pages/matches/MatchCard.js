4162d9041f8abc7c5695456ec7e2e3974afe6e7a
preston <prestonprince18@gmail.com>
2022-12-31 18:26:39 -0700

    added cancel match functionality

--- react-app/src/components/pages/matches/MatchCard.js
+++ react-app/src/components/pages/matches/MatchCard.js
@@ -1,14 +1,15 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
 import { useEffect } from "react";
 
+import { cancelMatch } from "../../../store/matches";
+
 const MatchCard = ({ match }) => {
     const [isUserMatch, setIsUserMatch] = useState(false)
-
+    const dispatch = useDispatch()
     const user = useSelector(state => state.session.user)
     const matchTeamIds = match.teams.map(team => team.id)
     const matchTeamOwnerIds = match.teams.map(team => team.owner_id);
-    console.log(matchTeamOwnerIds)
 
     let userTeams;
     if (user) {
@@ -25,17 +26,26 @@ const MatchCard = ({ match }) => {
         }
     }, [])
 
+    // const teamId = matchTeamIds.find(id => match.teams[0].id === id);
+    // console.log(matchTeamIds)
+    // console.log(teamId)
+
+    const handleCancel = () => {
+        dispatch(cancelMatch(match, matchTeamIds[0]))
+    }
+
 
     return (
         <div>
             <span>XP</span>
             <span>{match.type}</span>
             <span>{match.map}</span>
-            {(user && isUserMatch) && matchTeamOwnerIds.includes(user.id) ? (
-                <button>Cancel</button>
-            ):
+            {(user && isUserMatch) && matchTeamOwnerIds.includes(user.id) && (
+                <button onClick={handleCancel}>Cancel</button>
+            )}
+            {user && !isUserMatch && (
                 <button>Accept</button>
-            }
+            )}
         </div>
     )
 };

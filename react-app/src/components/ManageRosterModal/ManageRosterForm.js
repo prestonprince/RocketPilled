ea9138872bbc30d73a8078e307b0b8cc46ded55a
preston <prestonprince18@gmail.com>
2023-01-06 11:38:37 -0700

    added filter for adding tm8s

--- react-app/src/components/ManageRosterModal/ManageRosterForm.js
+++ react-app/src/components/ManageRosterModal/ManageRosterForm.js
@@ -5,11 +5,15 @@ import UserCard from "./UserCard";
 
 const ManageRosterForm = ({ team, setShowRosterModal }) => {
     const allUsers = useSelector(state => state.session.allUsers);
+    const teamMembersIds = team.members.map(player => player.id);;
+    const validUsers = allUsers
+    .filter(user => !teamMembersIds.includes(user.id))
+    .filter(user => user[team.type].length < 1)
 
     return (
         <div className={styles.container}>
             <div className={styles.cardContainer}>
-                {allUsers && allUsers.map(user => (<UserCard key={user.id} user={user} team={team} setShowRosterModal={setShowRosterModal} />))}
+                {allUsers && validUsers.map(user => (<UserCard key={user.id} user={user} team={team} setShowRosterModal={setShowRosterModal} />))}
             </div>
         </div>
     )

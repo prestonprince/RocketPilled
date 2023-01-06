6ab9de3964bfe50f7e044c6a17a2d40fd48289c0
preston <prestonprince18@gmail.com>
2023-01-06 12:17:11 -0700

    added about links

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -1,7 +1,22 @@
+import styles from './cssModules/Footer.module.css'
+
 const Footer = () => {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <div>
+                <span className={styles.title}>DEVELOPED BY PRESTON PRINCE</span>
+            </div>
+                <a href='https://github.com/prestonprince' target='_blank' className={styles.link}>
+                    <div className={styles.gitBox}>
+                        <div className={styles.iconContainer}>
+                            <img
+                                src="https://img.icons8.com/ios-glyphs/30/FFFFFF/github.png"
+                                alt="github_icon"
+                            />
+                        </div>
+                        {/* <span>GITHUB</span> */}
+                    </div>
+                </a>
         </div>
     )
 };

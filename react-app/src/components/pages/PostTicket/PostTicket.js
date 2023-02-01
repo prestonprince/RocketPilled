bd7aa0f017338dfdbe30869ac45457adfb06ea11
preston <prestonprince18@gmail.com>
2023-02-01 16:00:50 -0700

    started post ticket form

--- react-app/src/components/pages/PostTicket/PostTicket.js
+++ react-app/src/components/pages/PostTicket/PostTicket.js
@@ -19,7 +19,26 @@ function PostTicket() {
             </div>
             <div className={styles.formContainer}>
                 <form>
-                    <label htmlFor=''></label>
+                    <div className={styles.descriptionContainer}>
+                        <textarea
+                        className={styles.description}
+                        placeholder='Tell us how we can resolve this disputed match'
+                        value={description}
+                        onChange={(e) => setDescription(e.target.value)}
+                        >
+                        </textarea>
+                        <span className={description.length > 250 ? styles.redCharCount : styles.charCount}>{description.length} / 250</span>
+                    </div>
+                    <div className={styles.screenshotLinkContainer}>
+                        <input 
+                        className={styles.screenshotLink}
+                        type='text'
+                        value={screenshotLink}
+                        onChange={(e) => setScreenshotLink(e.target.value)}
+                        placeholder='Link to screenshot of match score here'
+                        />
+                    </div>
+                    <button className={styles.btn}>Submit</button>
                 </form>
             </div>
         </div>
